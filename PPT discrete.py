9ff21475b86a06cd1b138e08210a0f46f2d02aaa
marzel10 <maria.zelewska@gmail.com>
2024-10-22 09:41:41 +0200

    Add files via upload

--- PPT discrete.py
+++ PPT discrete.py
@@ -1,4 +1,4 @@
-from David_work import ThrusterOptimizer1,print_results
+from David_work import ThrusterOptimizer1,print_results #imported the thruster class from david work
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -32,37 +32,36 @@ constants = {
     "I_bit_max": 400,
 }
 
-THR=ThrusterOptimizer1(parameters,constants)
-ah=np.linspace(parameters["h"][0],parameters["h"][1],200)
-aw=np.linspace(parameters["w"][0],parameters["w"][1],200)
-Isp=[]
-ISP=0
-minw=[]
-H=0
-W=0
-ah1=[]
-aw1=[]
+THR=ThrusterOptimizer1(parameters,constants) #seting up the thruster optimizer
+ah=np.linspace(parameters["h"][0],parameters["h"][1],200) #creating an array with 200 values for height starting from 0.5 and ending on 5
+aw=np.linspace(parameters["w"][0],parameters["w"][1],200) #doing the same with width
+Isp=[] #an array where specific impulses (for each pair of h and w from ah and aw) will be stored
+ISP=0 #maximal specific impulse
+H=0 #height at maximal Isp
+W=0 #width at maximal Isp
+ah1=[] # an array where every height inputed into get_resulst is stored (in right order)
+aw1=[] # equivalent of ah1 for the width
 
-for i in range(len(ah)):
-    #for j in range(len(aw)):
-        pa=[ah[i],1,35,100,3]
-        isp=THR.get_results1(pa)[6]
+for i in range(len(ah)): #for every h from ah
+    for j in range(len(aw)): #for every w from aw
+        pa=[ah[i],aw[j],35,100,3] # thruster parameters
+        isp=THR.get_results1(pa)[6] #calculate specific impulse for these parameters
         Isp.append(isp)
-        ah1.append(ah[i])
-        #aw1.append(aw[j])
-        if isp>ISP:
+        ah1.append(ah[i]) #add used height (in pa) to ah1
+        aw1.append(aw[j]) #add used width (in pa) to ah1
+        if isp>ISP: #searching for the bigest specific impulse
             ISP=isp
             H=ah[i]
-            #W=aw[j]
+            W=aw[j]
 #
 
-print(ISP,H)
-param=[H,1,35,100,3]
-opt=THR.get_results1(param)
-print_results(param,opt)
+print(ISP,H,W) #print optimum parameters
+param=[H,W,35,100,3]
+opt=THR.get_results1(param) # calculate specifications for optimum parameters
+print_results(param,opt) #print all specifications for optimum parameters
 
 
-
+#making a scatter plot of every result
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -72,7 +71,7 @@ x = np.random.rand(100)
 y = np.random.rand(100)
 z = np.random.rand(100)
 
-#ax.scatter(ah1, aw1, Isp)
+ax.scatter(ah1, aw1, Isp)
 
 ax.set_xlabel('Height')
 ax.set_ylabel('Width')
