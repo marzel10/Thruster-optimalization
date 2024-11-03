3a5be159ebab7ba0a338faa2c15cd16a022160d2
Maria <maria.zelewska@gmial.com>
2024-11-03 17:20:27 +0100

    cleaning 2

--- PPT discrete.py
+++ PPT discrete.py
@@ -61,4 +61,4 @@ param=[H,W,35,100,3]
 opt=THR.get_results1(param) # calculate specifications for optimum parameters
 print_results(param,opt) #print all specifications for optimum parameters
 
-print("HHAH")
+
